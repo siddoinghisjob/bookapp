2e24cc368fc31b7abc0e197022a511e0b70e0f72
Soumya Deep Sarkar <96379749+siddoinghisjob@users.noreply.github.com>
2024-05-20 15:31:26 +0530

    trying logout

--- backend/controller/logout.js
+++ backend/controller/logout.js
@@ -3,10 +3,11 @@ const model = require("../model/logout");
 const logout = async (req, res) => {
   try {
     const success = await model(req.jwtid);
-    if (success) {
-      res.clearCookie("token");
-      res.json({success : true, message: "Successfully logged out" }).status(200);
-    } else throw new Error("Server error");
+    if (!success) {
+      throw new Error();
+    }
+    res.clearCookie("token");
+    res.json({ success: true, test : req.jwtid , message: "Successfully logged out" }).status(200);
   } catch (e) {
     res.json({ message: "Server Error" }).status(500);
   }
